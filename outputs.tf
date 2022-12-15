d8e3e5ce99d3431023d488868ea25dd1b59cd574
Pablo Arango <pablo.arango@pyxisportal.co>
2022-12-15 12:49:13 +0000

    v0.1.1

---
+++ outputs.tf
@@ -0,0 +1,9 @@
+output "s3_bucket_id" {
+  description = "The name of the bucket."
+  value       = element(concat( module.s3_bucket.*.s3_bucket_id, [""]), 0)
+}
+
+output "s3_bucket_arn" {
+  description = "The ARN of the bucket. Will be of format arn:aws:s3:::bucketname."
+  value       = element(concat( module.s3_bucket.*.s3_bucket_arn, [""]), 0)
+}
